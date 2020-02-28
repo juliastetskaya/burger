997894eac7c1b487324055d6dfe5434afa5c3edd
Iuliia Stetskaia <iuliia_stetskaia@epam.com>
2020-02-28 14:05:37 +0300

    add a dynamic ingredient component

---
+++ src/components/Burger/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import BurgerIngredient from './BurgerIngredient';
+
+const Burger = () => (
+    <>
+        <BurgerIngredient type='bread-bottom' />
+        <BurgerIngredient type='bread-top' />
+    </>
+);
+
+export default Burger;
--- src/containers/BurgerBuilder.jsx
+++ src/containers/BurgerBuilder.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import Burger from '../components/Burger';
 
 const BurgerBuilder = () => (
     <>
-        <div>Burger</div>
+        <Burger />
         <div>Build Controls</div>
     </>
 );
