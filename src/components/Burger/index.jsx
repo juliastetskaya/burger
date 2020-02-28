099507c78cc2c66192f9422abe056392665a1454
Iuliia Stetskaia <iuliia_stetskaia@epam.com>
2020-02-28 15:08:25 +0300

    start the Burger component

--- src/components/Burger/index.jsx
+++ src/components/Burger/index.jsx
@@ -1,11 +1,40 @@
 import React from 'react';
+import styled from '@emotion/styled';
+
 import BurgerIngredient from './BurgerIngredient';
 
+const WrapperStyled = styled.div`
+    width: 100%;
+    margin: auto;
+    height: 250px;
+    overflow: overlay;
+    text-align: center;
+    font-weight: bold;
+    font-size: 1.2rem;
+
+    @media (min-width: 500px) and (min-height: 400px) {
+        width: 350px;
+        height: 300px;
+    }
+
+    @media (min-width: 500px) and (min-height: 401px) {
+        width: 450px;
+        height: 400px;
+    }
+
+    @media (min-width: 1000px) and (min-height: 700px) {
+        width: 700px;
+        height: 600px;
+    }
+`;
+
 const Burger = () => (
-    <>
-        <BurgerIngredient type='bread-bottom' />
+    <WrapperStyled>
         <BurgerIngredient type='bread-top' />
-    </>
+        <BurgerIngredient type='cheese' />
+        <BurgerIngredient type='meat' />
+        <BurgerIngredient type='bread-bottom' />
+    </WrapperStyled>
 );
 
 export default Burger;
