19dc1017fa773a4554034aa14ccdd9e694f58e39
Iuliia Stetskaia <iuliia_stetskaia@epam.com>
2020-03-04 09:04:49 +0300

    add a Hamburger icon

--- src/components/Navigation/SideDrawer/DrawerToggle.jsx
+++ src/components/Navigation/SideDrawer/DrawerToggle.jsx
@@ -1,7 +1,35 @@
 import React from 'react';
+import styled from '@emotion/styled';
+
+const WrapperStyled = styled.div`
+    width: 40px;
+    height: 100%;
+    display: flex;
+    flex-flow: column;
+    justify-content: space-around;
+    align-items: center;
+    padding: 10px 0;
+    box-sizing: border-box;
+    cursor: pointer;
+
+    @media (min-width: 500px) {
+        display: none;
+    }
+`;
+
+const BarStyled = styled.div`
+    width: 90%;
+    height: 4px;
+    border-radius: 2px;
+    background-color: white;
+`;
 
 const DrawerToggle = ({ clickHandler }) => (
-    <div onClick={clickHandler}>MENU</div>
+    <WrapperStyled onClick={clickHandler}>
+        <BarStyled />
+        <BarStyled />
+        <BarStyled />
+    </WrapperStyled>
 );
 
 export default DrawerToggle;
