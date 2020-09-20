94cb63d09beb7ab194abd3b328c83635effe64b9
Iuliia Stetskaia <iuliia_stetskaia@epam.com>
2020-09-20 16:50:38 +0300

    fetching orders

--- src/components/Order/index.jsx
+++ src/components/Order/index.jsx
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from '@emotion/styled';
 
+import axios from '../../axios';
+import withErrorHandler from '../../hoc/withErrorHandler';
+
 const OrderStyled = styled.div`
-    width: 100%;
+    width: 80%;
     border: 1px solid #eee;
     box-shadow: 0 2px 3px #ccc;
     padding: 10px;
@@ -10,11 +13,28 @@ const OrderStyled = styled.div`
     box-sizing: border-box;
 `;
 
-const Order = () => (
-    <OrderStyled>
-        <p>Ingredients: Salad(1)</p>
-        <p>Price: <strong>USD 5.45</strong></p>
-    </OrderStyled>
-);
+const Order = () => {
+    const [orders, setOrders] = useState([]);
+    const [loading, setLoading] = useState(true);
 
-export default Order;
+    useEffect(() => {
+        axios.get('orders.json')
+            .then(response => {
+                setOrders(Object.values(response.data));
+                setLoading(false);
+            })
+            .catch(error => {
+                console.error(error);
+                setLoading(false);
+            });
+    }, []);
+
+    return (
+        <OrderStyled>
+            <p>Ingredients: Salad(1)</p>
+            <p>Price: <strong>USD 5.45</strong></p>
+        </OrderStyled>
+    );
+};
+
+export default withErrorHandler(Order, axios);
